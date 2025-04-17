5bc3a1506619171121755762e92026dbc0131f57
Mo' Sheriff <smomoh96@gmail.com>
2025-04-17 19:53:58 +0100

    increased font size for project grid header

--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -8,7 +8,7 @@ interface ProjectGridProps {
 export default function ProjectGrid({ projects }: ProjectGridProps) {
   return (
     <div className="px-6 md:px-10 py-12">
-      <h2 className="uppercase text-center mb-6 text-xl tracking-wider font-adobe-caslon">SELECTION</h2>
+      <h2 className="uppercase text-center mb-6 text-xl lg:text-3xl tracking-wider font-adobe-caslon">SELECTION</h2>
       <div className="project-grid grid lg:grid-cols-3 sm:gap-0">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
