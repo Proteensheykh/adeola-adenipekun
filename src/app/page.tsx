124ea04edb8ebbebeb2de12a148ad8c4e9869109
Mo Sheriff <smomoh96@gmail.com>
2025-05-18 12:40:43 +0100

    rendered asset with cloudinary cdn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             loop
           >
             <source
-              src="https://media-hosting.imagekit.io/59f0ca26bfa140e1/header-vid.mp4?Expires=1839499744&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=MSaCWb4fPNkQB8W~1XGuo8pol2whL4YIfZHBH5z5dfDwoN1A6bgHnMSqD8fFGoVcRj9ik3ZZkYcrlqGdG2o0EtP8EY3biXpmwR8g6w0tBv~ApiddTfUmMvXp1IFTLY7N6nhjKHP2hrSG3BiCMBT-d14TB4YsQaxp8m21N-nZJGpforjpZX7HdBrLIIu1mJ~Stgs5LZZJwsJG6PJkdS-JErXz~Ny2F0xDWBFyecfbIflRNQM9vuAZYZlqNsZ-wB9tseXCazrykEDW9u2e~hDxsv4N7qug9r7HL8OV~5sg788xMFSO~prrSCm105vi89t8T45bfN~jKKnRL7kavR3mxg__"
+              src="https://res.cloudinary.com/cloud42/video/upload/v1747565312/copy_326ACB09-F799-4E1B-8D3B-944A7E7EC5BB_uhn4yv.mov"
               type="video/mp4"
             />
             Your browser does not support the video tag.
